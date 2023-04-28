4a8f95c3c8530299263e536aa3306ff71d95498b
marco <marcoeribes@gmail.com>
2023-04-28 15:13:05 -0700

    got adding new sticky notes to work

--- src/app/components/add-task-item/add-task-item.component.ts
+++ src/app/components/add-task-item/add-task-item.component.ts
@@ -10,7 +10,10 @@ import { Task } from '../../Task';
 })
 export class AddTaskItemComponent implements OnInit {
   @Output() onAddTask: EventEmitter<Task> = new EventEmitter();
-  showAddTask: boolean = true;
+  text: string;
+  day: string;
+  reminder: boolean = false;
+  showAddTask: boolean = false;
   subscription: Subscription;
 
   constructor(private uiService: UiService) {
@@ -26,4 +29,21 @@ export class AddTaskItemComponent implements OnInit {
     this.showAddTask = !this.showAddTask;
   }
 
+  onSubmit() {
+    if (!this.text) {
+      alert('Please add a task!');
+      return;
+    }
+    const newTask: Task = {
+      text: this.text,
+      day: this.day,
+      reminder: this.reminder,
+    };
+
+    this.onAddTask.emit(newTask);
+
+    this.text = '';
+    this.day = '';
+    this.reminder = false;
+  }
 }
